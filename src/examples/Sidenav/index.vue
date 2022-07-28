1e17560ee4e7bbd17865df071ce8b4cfaef929a3
aliefrahman02 <aliefrahman02@mail.ugm.ac.id>
2022-07-28 22:44:21 +0700

    first commit

---
+++ src/examples/Sidenav/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <aside
+    class="overflow-auto sidenav navbar navbar-vertical navbar-expand-xs"
+    :class="`${
+      this.$store.state.isRTL
+        ? 'me-3 rotate-caret fixed-end'
+        : 'fixed-start'
+    } 
+    ${
+      this.$store.state.layout === 'landing'
+        ? 'bg-transparent shadow-none'
+        : ' '
+    } ${this.$store.state.sidebarType}`"
+    id="sidenav-main"
+  >
+    <div class="sidenav-header center">
+      <i
+        class="top-0 p-3 cursor-pointer fas fa-times text-secondary opacity-5 position-absolute end-0 d-none d-xl-none"
+        aria-hidden="true"
+        id="iconSidenav"
+      ></i>
+      <router-link class="m-0 navbar-brand" to="/">
+        <img
+          :src="logofloo"
+          class="navbar-brand-img h-100 center"
+        />
+      </router-link>
+    </div>
+    <hr class="mt-0 horizontal dark" />
+    <sidenav-list :cardBg="custom_class" />
+  </aside>
+</template>
+<script>
+import SidenavList from "./SidenavList.vue";
+import logo from "@/assets/img/logo-ct-dark.png";
+import logoWhite from "@/assets/img/logo-ct.png";
+import logofloo from "@/assets/img/logo-floo.png";
+
+export default {
+  name: "index",
+  components: {
+    SidenavList
+  },
+  data() {
+    return {
+      logo,
+      logoWhite,
+      logofloo
+    };
+  },
+  props: ["custom_class", "layout"]
+};
+</script>
